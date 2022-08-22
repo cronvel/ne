9e295da89dbad62802bf612f2fb9b2514ea7c215
Cédric Ronvel <cedric.ronvel@gmail.com>
2022-08-22 11:59:49 +0200

    Packages upgraded

--- test/neon-test.js
+++ test/neon-test.js
@@ -1,8 +1,8 @@
 /*
-	The Cedric's Swiss Knife (CSK) - Neon test suite
+	Neon
 
-	Copyright (c) 2015 Cédric Ronvel 
-	
+	Copyright (c) 2015 - 2022 Cédric Ronvel
+
 	The MIT License (MIT)
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
